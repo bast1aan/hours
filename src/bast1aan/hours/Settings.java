1f45d9ca2ed247444c465f35500dc0f2274b878b
Bastiaan Welmers <bastiaan@welmers.net>
2016-07-03 23:28:37 +0200

    Basic Database functionality
    - Settings to provide settings from settings.properties
    - ConnectionManager to provide reliable SQL backend connection
    - Dao (Data Acces Object) to communicate between code and SQL backend

---
+++ src/bast1aan/hours/Settings.java
@@ -0,0 +1,91 @@
+/*
+ * Hours
+ * Copyright (C) 2016 Bastiaan Welmers, bastiaan@welmers.net
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package bast1aan.hours;
+
+import com.opensymphony.xwork2.util.ClassLoaderUtil;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class Settings {
+	
+	public final static String DRIVER = "driver";
+	public final static String DSN = "dsn";
+	public final static String USERNAME = "username";
+	public final static String PASSWORD = "password";
+	public final static String REMOTE_ADDR_HEADER = "remote_addr_header";
+	
+	private static Settings instance;
+	
+	public static Settings getInstance() {
+		if (instance == null) {
+			instance = new Settings();
+		}
+		return instance;
+	}
+	
+	private Map<String, String> settings = new HashMap<String, String>();
+	
+	public Settings() {
+		URL settingsUrl = ClassLoaderUtil.getResource("settings.properties", getClass());
+		
+		if (settingsUrl == null) {
+			throw new HoursException("settings.properties not found");
+		}
+		InputStream stream = null;
+		
+		Properties prop = new Properties();
+		try {
+			stream = settingsUrl.openStream();
+			prop.load(stream);
+			// load properties that are declared
+			Field[] fields = Settings.class.getFields();
+			for (Field field: fields) {
+				if (Modifier.isStatic(field.getModifiers()) && field.getType() == String.class) {
+					String key;
+					try {
+						key = (String) field.get(null);
+					} catch (IllegalAccessException e) {
+						throw new HoursException("Error reading field from Settings class", e);
+					}
+					settings.put(key, prop.getProperty(key));
+				}
+			}
+		} catch (IOException e) {
+			throw new HoursException("error reading settings.properties", e);
+		} finally {
+			if (stream != null) {
+				try {
+					stream.close();
+				} catch (IOException e) {
+					throw new HoursException("error closing settings.properties", e);
+				}
+			}
+		}
+
+	}
+
+	public String get(String key) {
+		return settings.get(key);
+	}
+}
